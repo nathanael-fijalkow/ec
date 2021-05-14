112a9587613d88aa50f9c9631113656c3c62d769
Nathanael Fijalkow <nathanael.fijalkow@gmail.com>
2021-05-14 20:52:49 +0200

    optimised DFS: not copy for partial programs

--- dreamcoder/PCFG/Algorithms/dfs.py
+++ dreamcoder/PCFG/Algorithms/dfs.py
@@ -7,7 +7,7 @@ import time
 def dfs(G : PCFG):
     '''
     A generator that enumerates all programs using a DFS.
-    Assumes that the PCFG only generates programs of bounded depth.
+    Assumes that the rules are non-increasing
     '''
     # chrono = -time.perf_counter()
     # We reverse the rules: they should be non-increasing
@@ -19,7 +19,7 @@ def dfs(G : PCFG):
     frontier = deque()
     initial_non_terminals = deque()
     initial_non_terminals.append(G.start)
-    frontier.append(([], initial_non_terminals))
+    frontier.append((None, initial_non_terminals))
     # A frontier is a queue of pairs (partial_program, non_terminals) describing a partial program:
     # partial_program is the list of primitives and variables describing the leftmost derivation, and
     # non_terminals is the queue of non-terminals appearing from left to right
@@ -27,14 +27,13 @@ def dfs(G : PCFG):
     while len(frontier) != 0:
         partial_program, non_terminals = frontier.pop()
         if len(non_terminals) == 0: 
-            # print(partial_program)
             yield partial_program
         else:
             S = non_terminals.pop()
             for F, args_F, w in G.rules[S]:
-                new_partial_program = partial_program.copy()
-                new_partial_program.append(F)
+                new_partial_program = (F, partial_program)
                 new_non_terminals = non_terminals.copy()
                 for arg in args_F:
                     new_non_terminals.append(arg)
                 frontier.append((new_partial_program, new_non_terminals))
+
